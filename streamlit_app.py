4eb6a9268e931dba835a1b562fd019d179c92415
tanujkapila <tanujkapila1041@gmail.com>
2022-06-17 10:54:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.caption('Omega 3 & Blueberry Oatmeal')
+streamlit.caption('Kale, Spinach & Rocket Smoothie')
+streamlit.caption('Hard-Boiled Free-Range Egg')
 
-streamlit.title(<H3>'Breakfast Menu'</H3>)
